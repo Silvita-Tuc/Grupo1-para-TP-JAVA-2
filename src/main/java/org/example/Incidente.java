c8b0521363f9f29e5810a9a8ea5dc374b96fd616
Dario Lopez <26413@sistemas.frc.utn.edu.ar>
2023-11-09 00:20:38 -0300

    Creacion de clase tecnico, e Incidente para el diagrama de clases

---
+++ src/main/java/org/example/Incidente.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Incidente {
+    //horas {complejo,}
+    private int codigo;
+    private String servicio;
+    private String problema;
+  // public boolean isResuelto();
+}
---
+++ src/main/java/org/example/Tecnico.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Tecnico {
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private String Estado;
+}
