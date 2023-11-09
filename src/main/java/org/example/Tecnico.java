b70f7321afa9c054cd8a080c42994e18ed94a496
Dario Lopez <26413@sistemas.frc.utn.edu.ar>
2023-11-09 01:58:32 -0300

    Modificacion de clase tecnico, e Incidente

--- src/main/java/org/example/Tecnico.java
+++ src/main/java/org/example/Tecnico.java
@@ -4,5 +4,54 @@ public class Tecnico {
     private int codigo;
     private String nombre;
     private String apellido;
-    private String Estado;
+    private String estado;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Tecnico(int codigo, String nombre, String apellido, String estado) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Tecnico{" +
+                "codigo=" + codigo +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", estado='" + estado + '\'' +
+                '}';
+    }
 }
